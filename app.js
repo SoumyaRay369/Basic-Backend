7e34acef0888ec525ea4aedf28e9e3e51ddc17ff
Soumya Ray <92206768+SoumyaRay369@users.noreply.github.com>
2023-10-26 02:21:06 +0530

    Complete update of app.js with new connection of Database

--- app.js
+++ app.js
@@ -1,14 +1,69 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000 || process.env.PORT;
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error(err);
+});
+
+// Define a schema for the user data
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+// Define a model for the user data
+const User = mongoose.model('User', userSchema);
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send("Hello World")
-})
+    res.sendFile(__dirname + '/index.html');
+});
 
-app.listen(PORT, ()=> {
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    // Save the user data to MongoDB
+    const user = new User({ username, password });
+    user.save().then(() => {
+        res.send('Login successful');
+    }).catch((err) => {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    });
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/login.html');
+});
+
+app.post('/authenticate', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    // Check if the user exists in MongoDB
+    User.findOne({ username, password }).then((user) => {
+        if (user) {
+            res.send('Login successful');
+        } else {
+            res.send('Invalid username or password');
+        }
+    }).catch((err) => {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    });
+});
+
+app.listen(PORT, () => {
     console.log('Listening on port 3000')
 })
